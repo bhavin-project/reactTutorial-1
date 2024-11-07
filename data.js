fea6c5307c1e15a4732a803251eb261ec7356b5f
bhavin-project <bhavin.project@gmail.com>
2024-11-07 17:51:28 +0530

    React2nd

---
+++ data.js
@@ -0,0 +1,23 @@
+export const data = [
+  { id: 1, name: "Mohit" },
+  { id: 2, name: "Prem" },
+  { id: 3, name: "Suresh" },
+  { id: 4, name: "Anna" },
+];
+
+export const people = [
+  { id: 1, name: "Borish", nickName: "Bot" },
+  { id: 2, name: "Yashvi" },
+  {
+    id: 3,
+    name: "oliver",
+    images: [
+      {
+        small: {
+          url: "https://res.cloudinary.com/diqqf3eq2/image/upload/ar_1:1,bo_5px_solid_rgb:ff0000,c_fill,g_auto,r_max,w_1000/v1595959121/person-1_aufeoq.jpg",
+        },
+      },
+    ],
+  },
+  { id: 4, name: "Kabir" },
+];
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import Book1 from "./book-1.jsx"; // for 1
+import Book2 from "./book-2.jsx"; // for 2
+import Book3 from "./book-3.jsx"; // for 3
+import Book4 from "./book-4.jsx"; // for 3
+const Greetings1 = () => {
+  return (
+    <>
+      <h1>Hello from 1st component</h1>;
+      <Person />
+      <Message />
+    </>
+  );
+};
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const Person = () => (
+  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}>
+    Hiren Shahoo
+  </h4>
+);
+const Message = () => {
+  return <p>this is my message</p>;
+};
+
+// createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(<Book4 />);
+// createRoot(document.getElementById("root")).render(<Greetings1 />);
